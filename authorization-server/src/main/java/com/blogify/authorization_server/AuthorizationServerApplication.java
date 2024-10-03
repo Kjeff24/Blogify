5a83e5cf56f031f4c1ff52198c69f20729751dae
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2024-10-03 11:04:32 +0000

    pom separation

--- authorization-server/src/main/java/com/blogify/authorization_server/AuthorizationServerApplication.java
+++ authorization-server/src/main/java/com/blogify/authorization_server/AuthorizationServerApplication.java
@@ -3,6 +3,8 @@ package com.blogify.authorization_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class AuthorizationServerApplication {
 
